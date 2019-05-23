520e12601e0abe3d5200760dacebe1001be48aca
Trista G. Paul <tristagpaul@protonmail.com>
2019-05-23 13:05:07 -0700

    Added new functions (clean_tweet_regression, clean_timeline_regression, regression) that comprise stretch model

--- DSFunctions.py
+++ DSFunctions.py
@@ -13,6 +13,12 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
+import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 def process_tweet(tweet):
   '''converts a single tweet into a form understandable by predictor.'''
@@ -85,3 +91,108 @@ def score_timeline(timeline):
    score = process_tweet(tweet)
    scores.append(score)
  return scores
+
+def clean_tweet_regression(tweet):
+  '''converts a single tweet into a form understandable by predictor.'''
+  '''variation of existing cleaner to clean punct and'''
+  '''return string rather than score'''
+  
+  #table that replaces punct with empty space
+  table = str.maketrans('', '', punctuation)
+  
+  #nltk stopwords are words too common to be useful
+  #not stopwords as in profanity
+  stop_words = stopwords.words('english')
+  
+  #convert to string
+  tweet = str(tweet)
+  
+  #remove urls
+  url = re.compile('http.+? ')
+  tweet = re.sub(url, '', tweet)
+  
+  #remove tagged users
+  user = re.compile('@.+? ')
+  tweet = re.sub(user, "", tweet)
+  
+  #clean html tags and literals
+  tag = re.compile('<.*?>')
+  tweet = re.sub(tag, '', tweet)
+  tweet = tweet.replace(r"\n", " ")
+  tweet = tweet.replace(r"b'", "")
+  tweet = tweet.replace(r"\r", "")
+  
+  #temp. tokenize to clean on word level
+  words = word_tokenize(tweet)
+  
+  #remove spaces (will be added in again)
+  words = [word.strip() for word in words]
+  
+  #lowercase
+  words = [word.lower() for word in words]
+  
+  #remove non alphanumeric characters
+  words = [word.translate(table) for word in words]
+  words = [word for word in words if word.isalpha()]
+  
+  #remove stopwords
+  words = [word for word in words if not word in stop_words]
+  
+  #lemmatize (remove prefixes and suffixes)
+  lemmatizer = WordNetLemmatizer()
+  words = [lemmatizer.lemmatize(word) for word in words]
+  fin_tweet = " ".join(words)
+  
+  return fin_tweet
+
+def clean_timeline_regression(timeline):
+  '''loop of clean_tweet_regression'''
+  '''runs as part of the function regression'''
+  clean = []
+  for tweet in timeline:
+    fin_tweet = clean_tweet_regression(tweet)
+    clean.append(fin_tweet)
+  return clean
+
+def regression(tweets, likes, new_tweet):
+  '''trains a ridge regression live with the users timeline.
+     can accept any metadata variable that is a number.
+     stretch goal: make a multivariate regression'''
+  
+  #get training data
+  clean_tweets = clean_timeline_regression(tweets)
+  vectorizer = CountVectorizer()
+  vectorizer.fit(clean_tweets)
+  word_counts = vectorizer.transform(clean_tweets)
+  X = pd.DataFrame(word_counts.toarray(), columns=vectorizer.get_feature_names())
+  X['target'] = likes
+  y = X['target']
+  X = X.drop(columns='target')
+  Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.1)
+  
+  #fit Ridge; evaluate what alpha results in lowest loss
+  alphas = []
+  mses = []
+  
+  ridge_reg = Ridge().fit(X, y)
+  
+  for alpha in range(0, 200, 1):
+      ridge_reg_split = Ridge(alpha=alpha).fit(Xtrain, ytrain)
+      mse = mean_squared_error(ytest, ridge_reg_split.predict(Xtest))
+      alphas.append(alpha)
+      mses.append(mse)
+  
+  #lowest mse in list and its index
+  min_index = mses.index(min(mses))
+  
+  #fit training data to min_index alpha
+  ridge_reg_split = Ridge(alpha=min_index).fit(Xtrain, ytrain)
+  
+  #clean the new tweet
+  cl = [clean_tweet_regression(new_tweet)]
+  new_array = vectorizer.transform(cl).toarray()
+  
+  #return a prediction for new tweet
+  pred = int(ridge_reg_split.predict(new_array))
+  return pred
+  
